dc000e173faca60eb3478e25fab7fd65fea543b5
Bruna <brunaosti@alunos.utfpr.edu.br>
2017-11-14 02:24:01 -0200

    Add files via upload

--- mochila.h
+++ mochila.h
@@ -1,25 +1,32 @@
 #include <stdio.h>
+int capMochila(int num1, int num2); int maiorvalor(int quantidade, int valor[], int peso[], int capacidade);
 
+// Utilizando-se de programação dinamica
 
-int tamMax(int num1, int num2){
+int capMochila(int num1, int num2){
 	return (num1 > num2)? num1 : num2;
 }
 
-int maiorvalor(int capacidade, int peso[], int valor[], int qt_itens){
+int maiorvalor(int quantidade, int valor[], int peso[], int capacidade){
 
 	int i, j;
-   	int mochila[qt_itens+1][j+1];
- 
-   for (i = 0; i <= qt_itens; i++){
-       for (j = 0; j <= capacidade; j++){
-           if (i==0 || j==0)
-               mochila[i][j] = 0;
-           else if (peso[i-1] <= j)
-                 mochila[i][j] = tamMax(valor[i-1] + mochila[i-1][capacidade-peso[i-1]],  mochila[i-1][j]);
+	capacidade++;
+	int** mochila = (int**)malloc(sizeof(int*)*(++quantidade));
+	for(i = 0; i < quantidade; i++){
+        mochila[i] = (int*)malloc(sizeof(int));
+        for(j = 0; j < capacidade; j++){
+            mochila[i][j] = 0;
+        }
+	}
+
+   for (i = 0; i < quantidade; i++){
+       for (j = 0; j < capacidade; j++){
+           if (peso[i-1] <= j)
+                 mochila[i][j] = capMochila(valor[i-1] + mochila[i-1][capacidade-peso[i-1]],  mochila[i-1][j]);
            else
                  mochila[i][j] = mochila[i-1][j];
        }
    }
- 
-   return mochila[qt_itens][j];
+
+   return mochila[quantidade][j];
 }
